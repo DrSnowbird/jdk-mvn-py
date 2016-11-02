cf09dba30d0ade8aee5524e9d65a61a385770237
DrSnowBird <drsnowbird@mail.com>
2016-11-01 22:11:21 -0400

    update commit-push.sh Tue Nov  1 22:11:20 EDT 2016

--- commit-push.sh
+++ commit-push.sh
@@ -3,16 +3,21 @@
 # Reference: https://docs.docker.com/engine/userguide/containers/dockerimages/
 
 echo "Usage: "
-echo "  ${0} <repo-name/repo-tag>"
+echo "  ${0} <comment> <repo-name/repo-tag>"
 echo
-imageTag=${1:-openkbs/jre-mvn-py}
+echo "-- Make sure you do login first: "
+echo "   docker login"
+echo
+
+comment=${1:-Update JVM}
+imageTag=${2:-openkbs/jre-mvn-py}
 
 docker ps -a
 
-containerID=`docker ps |grep "${imageTag}"|awk '{print $1}'`
+containerID=`docker ps |grep "${imageTag} "|awk '{print $1}'`
 echo "containerID=$containerID"
 
-docker commit -m "initial image" ${containerID} ${imageTag}
+docker commit -m "$comment" ${containerID} ${imageTag}
 
 docker push ${imageTag}
 
